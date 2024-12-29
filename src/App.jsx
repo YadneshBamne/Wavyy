0b06b6e2e4b53678c88189320c7448b1d18890e6
Bharat Sharma <bharatsharma98971@gmail.com>
2024-12-29 14:49:26 +0530

    Add routing and new Navbar component; include Login page and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import { Navbar } from './Navbar/Navbar';
-import { ManageTeam } from './Navbar/teamM/manageteam';
+import { Navbar } from './Pages/Components/Navbar';
+import { ManageTeam } from './Pages/Management/Manageteam';
+import { Bookings } from './Pages/Bookings/Bookings';
+import { Login } from "./Pages/Login/Login";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
 
 export default function TeamPage() {
   return (
     <div className="min-h-screen bg-gray-100">
-      <Navbar />
-      <main className="p-8">
+      {/* <main className="p-8">
         <ManageTeam />
-      </main>
+      </main> */}
+      <Router>
+      <Routes>
+        <Route path="/manage-team" element={<ManageTeam />} />
+        <Route path="/bookings" element={<Bookings />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
     </div>
   );
 }
